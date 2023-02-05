ea4c903534dd1a56ba7f6e9b9d848dc6bf566623
suckknowledge <124077161+suckknowledge@users.noreply.github.com>
2023-02-05 21:39:39 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-int my_strlen(const char * arr)
+size_t my_strlen(const char* arr[])//因为长度不可能是负数，所以用无符号整形 ，size_t 表示unsigned int
 {
-	int count = 0;
+	assert(arr);
+	size_t count = 0;
 	while (*arr++)
 	{
 		count++;
@@ -12,7 +12,7 @@ int main()
 {
 	int a = 0;
 	char arr[] = "1233334";
-	a = my_strlen(&arr);
+	a = my_strlen(arr);
 	printf("%d", a);
 	return 0;
 }
